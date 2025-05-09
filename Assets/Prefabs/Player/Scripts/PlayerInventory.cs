98c74e72fa73a462ab1b7efd0fdf913084595ba6
heitorpagotto <heitor.pagotto@outlook.com>
2025-05-08 21:21:51 -0300

    added coin anim and cinemachine

---
+++ Assets/Prefabs/Player/Scripts/PlayerInventory.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    private int MaxHealth = 2;
+    private int CurrentHealth = 2;
+
+    private int Coins = 0;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
