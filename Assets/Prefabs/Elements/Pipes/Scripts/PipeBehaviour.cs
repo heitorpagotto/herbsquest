f676fa3efc141083dd3e347db6b4774651746726
heitorpagotto <heitor.pagotto@outlook.com>
2025-05-29 20:28:22 -0300

    made level 3

--- Assets/Prefabs/Elements/Pipes/Scripts/PipeBehaviour.cs
+++ Assets/Prefabs/Elements/Pipes/Scripts/PipeBehaviour.cs
@@ -6,8 +6,10 @@ public class PipeBehaviour : MonoBehaviour
 {
     [SerializeField]
     [CanBeNull]
-    private Transform destination;
+    private PipeBehaviour destination;
 
+    
+    
     public void Teleport(GameObject player)
     {
         if (destination == null) return;
@@ -18,41 +20,54 @@ public class PipeBehaviour : MonoBehaviour
         playerController.enabled = false;
 
         var playerTransform = player.transform;
-        var initialPosition = playerTransform.position;
 
-        capsuleCollider.enabled = false;
+        //capsuleCollider.enabled = false;
 
         //rigidBody2D.bodyType = RigidbodyType2D.Kinematic;
-        rigidBody2D.gravityScale = 0;
+        //rigidBody2D.gravityScale = 0;
 
         // TODO: arrumar transform
-        
-        var targetPositionAnim = new Vector3(initialPosition.x, initialPosition.y - 100f, initialPosition.z);
-        playerTransform.position = Vector3.MoveTowards(initialPosition, targetPositionAnim, 10f * Time.deltaTime);
-        
-        // while (Vector3.Distance(playerTransform.position, targetPositionAnim) > 0.01f)
-        // {
-        //     playerTransform.position = Vector3.MoveTowards(initialPosition, targetPositionAnim, 10f * Time.deltaTime);
-        //     yield return null;
-        // }
 
-        StartCoroutine(AwaitForTeleportation(playerTransform, capsuleCollider, rigidBody2D, playerController));
+        var pipeCollider = gameObject.GetComponent<BoxCollider2D>();
+        pipeCollider.excludeLayers = -1;
+        
+        pipeCollider.enabled = false;
+
+        // var scale = player.transform.localScale;
+        // scale.y = 0.5f;
+        // player.transform.localScale = scale;
+
+
+        StartCoroutine(AwaitForTeleportation(playerTransform, capsuleCollider, rigidBody2D, playerController, pipeCollider));
     }
 
-    IEnumerator AwaitForTeleportation(Transform playerPosition, CapsuleCollider2D capsuleCollider, Rigidbody2D rigidBody, PlayerController controller)
+    IEnumerator AwaitForTeleportation(Transform playerPosition, CapsuleCollider2D capsuleCollider, Rigidbody2D rigidBody, PlayerController controller, BoxCollider2D pipeCollider)
     {
+        // var initialPosition = playerPosition.position;
+        //
+        // var targetPositionAnim = new Vector3(initialPosition.x, initialPosition.y - 10f, initialPosition.z);
+        // playerPosition.position = Vector3.MoveTowards(initialPosition, targetPositionAnim, 100f * Time.deltaTime);
+        //
+        // while (Vector3.Distance(playerPosition.position, targetPositionAnim) > 0.01f)
+        // {
+        //     playerPosition.position = Vector3.MoveTowards(initialPosition, targetPositionAnim, 10f * Time.deltaTime);
+        //     yield return null;
+        // }
+        //
         yield return new WaitForSeconds(1);
         
-        playerPosition.position = destination!.position;
+        playerPosition.position = destination!.transform.position;
 
-        var targetPositionAnim = new Vector3(playerPosition.position.x, playerPosition.position.y + 10f);
-        playerPosition.position = Vector3.MoveTowards(playerPosition.position, targetPositionAnim, 10 * Time.deltaTime);
+        var targetFinalPositionAnim = new Vector3(playerPosition.position.x, playerPosition.position.y + 100f);
+        playerPosition.position = Vector3.MoveTowards(playerPosition.position, targetFinalPositionAnim, 100f * Time.deltaTime);
         
         yield return new WaitForSeconds(1);
         
         controller.enabled = true;
         capsuleCollider.enabled = true;
         rigidBody.gravityScale = 1;
+        pipeCollider.enabled = true;
+        pipeCollider.excludeLayers = 0;
         //rigidBody.bodyType = RigidbodyType2D.Dynamic;
     }
 }
